3bc19014d6572c1bc1939501718a6a4f9ddf06f9
omnomdom <v.rabetskii@gmail.com>
2024-11-04 23:47:19 +0700

    add: add 1.5

---
+++ sem2/lab1/1.5/main.c
@@ -0,0 +1,89 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+
+// Обработчик для SIGINT
+void handler(int sig) {
+    printf("Second thread received SIGINT: %d\n", sig);
+}
+
+// Первый поток: Блокирует все сигналы
+void *thread1(void *arg) {
+    sigset_t mask;
+    // Заполняем маску всеми сигналами
+    sigfillset(&mask);  
+    // Блокируем все сигналы
+    pthread_sigmask(SIG_BLOCK, &mask, NULL);  
+    printf("First thread blocking all signals\n");
+
+    sleep(10);
+    printf("First thread has finished its work\n");
+    return NULL;
+}
+
+// Второй поток: Устанавливает обработчик для SIGINT
+void *thread2(void *arg) {
+    struct sigaction sa;
+    // Устанавливаем обработчик
+    sa.sa_handler = handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    // Назначаем обработчик для SIGINT
+    sigaction(SIGINT, &sa, NULL);
+    printf("Second thread waiting for SIGINT...\n");
+
+    sleep(10);
+    return NULL;
+}
+
+// Третий поток: Ожидает сигнал SIGQUIT с помощью sigwait
+void *thread3(void *arg) {
+    sigset_t mask;
+    int sig;
+
+    // Очищаем маску
+    sigemptyset(&mask);  
+    // Добавляем SIGQUIT в маску
+    sigaddset(&mask, SIGQUIT);  
+    // Блокируем SIGQUIT, чтобы использовать sigwait
+    pthread_sigmask(SIG_BLOCK, &mask, NULL);  
+
+    printf("Third thread is waiting for SIGQUIT...\n");
+    // Ожидаем SIGQUIT
+    sigwait(&mask, &sig);  
+
+    printf("Third thread received SIGQUIT signal: %d\n", sig);
+    return NULL;
+}
+
+int main() {
+    pthread_t tid1, tid2, tid3;
+
+    printf("Main thread PID: %d\n", getpid());
+
+    // Создаём потоки
+    pthread_create(&tid1, NULL, thread1, NULL);
+    pthread_create(&tid2, NULL, thread2, NULL);
+    pthread_create(&tid3, NULL, thread3, NULL);
+
+    
+    sleep(2);
+
+    // Отправляем SIGINT второму потоку
+    pthread_kill(tid2, SIGINT);  
+    sleep(1);
+    // Отправляем SIGQUIT третьему потоку
+    pthread_kill(tid3, SIGQUIT);  
+
+    pthread_join(tid1, NULL);
+    pthread_join(tid2, NULL);
+    pthread_join(tid3, NULL);
+
+    return 0;
+}
---
+++ sem2/lab1/1.5/pthread_kill_check.c
@@ -0,0 +1,53 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <string.h>
+#include <errno.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <signal.h>
+void handler1(int sig) {
+    printf("Thread 1 received signal: %d\n", sig);
+}
+
+void handler2(int sig) {
+    printf("Thread 2 received signal: %d\n", sig);
+}
+
+void *thread_func1(void *arg) {
+    struct sigaction sa;
+    sa.sa_handler = handler1;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGUSR1, &sa, NULL); // Устанавливаем handler1 для SIGUSR1
+    sleep(3); 
+    return NULL;
+}
+
+void *thread_func2(void *arg) {
+    struct sigaction sa;
+    sa.sa_handler = handler2;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+    sigaction(SIGUSR1, &sa, NULL); // Устанавливаем handler2 для SIGUSR1
+    sleep(3); // Ожидание сигнала
+    return NULL;
+}
+
+int main() {
+    pthread_t t1, t2;
+    pthread_create(&t1, NULL, thread_func1, NULL);
+    pthread_create(&t2, NULL, thread_func2, NULL);
+
+    // Отправляем сигнал конкретному потоку
+    sleep(1);
+    pthread_kill(t1, SIGUSR1);  // Ожидается вызов handler1
+    sleep(1);
+    pthread_kill(t2, SIGUSR1);  // Ожидается вызов handler2
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
+    return 0;
+}
+
